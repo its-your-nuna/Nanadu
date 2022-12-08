9cf3204543607b72ca736d5fbbc61fc0576b0568
its-your-nuna <200103140@stu.sdu.edu.kz>
2022-12-08 18:22:00 +0600

    change mui

--- src/components/home/AboutSection.js
+++ src/components/home/AboutSection.js
@@ -88,7 +88,17 @@ export default function AboutSection() {
   } = useStyles();
   return (
     <div >
-      <Typography className={about_title} component="h1">
+      <Typography sx={{
+        fontFamily: 'Inter, sans-serif',
+        fontSize: '3rem',
+        fontWeight: 'bold',
+        marginTop: '50px',
+        textAlign: 'center',
+        color:'#202794',
+        '@media (max-width: 500px)': {
+          fontSize: '2.5rem',
+        },
+      }} component="h1">
           Сертификаты
         </Typography>
         <div className={about_section}>
@@ -96,10 +106,29 @@ export default function AboutSection() {
       <div className={about_section_left}></div>
       <div className={about_section_right}>
         
-        <Typography className={leaderBoard_left_h1} variant="h2" component="h1">
+        <Typography sx={{
+          fontFamily: 'Inter, sans-serif',
+          fontSize: '3rem',
+          fontWeight: 'bold',
+          marginTop: '50px',
+          color:'black',
+          '@media (max-width: 900px)': {
+            fontSize: '2rem',
+          },
+        }}variant="h2" component="h1">
         Мы очень горды, 
         </Typography>
-        <Typography className={about_descriptions} component="p">
+        <Typography sx={{
+           width:'40vw',
+           fontFamily: 'Inter, sans-serif',
+           color:'grey',
+           fontSize: '1.5rem',
+           marginTop: '25px',
+           fontWeight: 'bold',
+           '@media (max-width: 900px)': {
+             fontSize: '1.2rem',
+           },
+        }} component="p">
         что сеть «Korean Street Food»-это казахстанская сеть общественного питания! Наша сеть имеет запатентованный зарегистрированный товарный знак. 
         </Typography>
         {/* <Typography className={about_descriptions} component="p">
